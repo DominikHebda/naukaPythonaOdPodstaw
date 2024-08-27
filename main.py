8302a10ab8934b64bd9333c35f010b1120f98758
DominikHebda <hebdad@wp.pl>
2024-08-27 14:43:04 +0200

    excersise 06 exceptions

--- main.py
+++ main.py
@@ -1,60 +1,117 @@
-def str_lenght(str1):
-    count = 0
-    for char in str1:
-        count += 1
-    return count
-
-print((str_lenght('Python')))
-
-
-Lista = [1, 2, -8]
-lst = [2, 3, 5]
+while True:
+    try:
+        num1 = int(input('Liczba 1:'))
+        num2 = int(input('Liczba 2:'))
+        print(f'Suma to: {num1+num2}')
+        break
+    except ValueError:
+        print('Wprowadź dane liczbowe')
 
 
-def sum_el_list(lst):
-    sum_list = 0
-    for el in lst:
-        sum_list += el
-    return sum_list
+
+a = 5
+b = 0
+try:
+    result = a / b
+except ZeroDivisionError:
+    result = "Nie możesz podzielić przez 0"
+
+print(result)
 
 
-print(sum_el_list(Lista))
-print(sum_el_list(lst))
+a = [1, 2, 5]
+try:
+    print(a[3])
+except IndexError:
+    pass
 
 
-def multiple_list(arg):
-    multi_list = 1
-    for el in arg:
-        multi_list *= el
-    return multi_list
+text = 'abc2'
+try:
+    msg = text + 5
+except TypeError:
+    msg = "Nie możesz dodać int do str"
+
+print(msg)
 
 
-print(multiple_list(Lista))
-print(multiple_list(lst))
+list = [2, 3, 4]
+
+try:
+    msg = list[5]
+except IndexError as error:
+    msg = "Jesteś poza zakresem listy (" + str(error) + ")"
+print(msg)
 
 
-list = [1, 2, -8, 0]
+a = int(input('Podaj pierwszą liczbę:'))
+b = int(input('Podaj drugą liczbę:'))
 
 
-def max_number(numbers):
-    max= numbers[0]
-    for num in numbers:
-        if num > max:
-            max = num
-    return max
+def division(a, b):
+    try:
+       divide = a / b
+    except ZeroDivisionError as e:
+        print(f"Wystąpił błąd:{e}")
+    else:
+        print(f"Wynik dzielenia to: {divide}")
 
 
-print(max_number(list))
+print(division(a, b))
+
+z = int(input("Podaj cenę netto:"))
+v = 1.23
 
 
-def fib(n):
+def multiply_Vat(z, v):
+    try:
+        if z <= 0:
+            raise ValueError("Cena nie może być ujemna!")
+        multiply = z * v
+        print(f"Cena brutto: {multiply} zł")
+    except ValueError as e:
+        print(f"Wystąpił błąd: {e}")
+    finally:
+        print("Obliczenie zakończone")
 
-    if n <= 1:
-        return n
-    return fib(n - 1) + fib(n - 2)
+multiply_Vat(z, v)
 
 
-for i in range(8):
-    print(fib(i))
+c = int(input('Podaj pierwszą liczbę:'))
+d = int(input('Podaj drugą liczbę:'))
+
+
+def division(a, b):
+    try:
+       divide = c / d
+    except ZeroDivisionError as e:
+        print(f"Wystąpił błąd:{e}")
+    else:
+        print(f"Wynik dzielenia to: {divide}")
+    finally:
+        print("obliczenie zakończone")
+
+
+print(division(c, d))
+
+
+def process_sensor_data(value_s, type_s):
+    try:
+        if not isinstance(value_s, (int, float)):
+            raise TypeError ("Wartość musi być liczbą")
+        if type_s not in ["temperatura", "ciśnienie", "wilgotność"]:
+            raise ValueError ("Nieznany typ sensora")
+    except Exception as e:
+        print((f"Wartość musi być liczbą: {e}"))
+    else:
+        print(f"Wartość sensora to: {value_s}, Typ sensora to: {type_s}")
+    finally:
+        print("Przetwarzanie zakończone")
+
+
+process_sensor_data(25.5, "temperatura")
+process_sensor_data("niepr", "ciśnienie")
+process_sensor_data(1010.5, 'Niezna')
+
 
 
